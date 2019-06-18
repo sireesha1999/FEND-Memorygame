438e552a753bdeb828e93f771ab03b3ab767941f
Sireesha1999 <mitta.sire@gmail.com>
2019-06-18 16:30:41 +0530

    files are modified

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# FEND-Memorygame
+# Memory Game Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+------------
+## I did the following steps in order to complete this Project
+### project is downloaded
+while reading the instructions ,I found a GitHub link which gives a brief idea about the project which is provide by **udacity**.
+I opened the link in chrome and downloaded the ZIP file and extract that file.
+After extracting the zip file I get the following files. They are:
+1. `css/app.css`
+2. `img/geometry2.png`
+3.  `js/app.js`
+4. `index.html`(strating file)
+5. `readme.md`files
+## Steps followed by me in order to finish the project
+I observed the code given by udacity in all the files .Finally I observe that mode of the modifications will be occur in app.js file .
++ In that file udacity provided **shuffle** function from stack overflow in order to shuffle the cards provided in the memory game.
++ all the shuffled cards are stored in the array called `changedCards`.After the cards are shuffled I just want to create a timer in order to display the timer in the game.
++ I had taken required variables in order to display the timer like **sec,min,hrs** and by using **innerHTML** for variable time I display the timer.
++ After that I added addEventListeners to all the shuffled cards. I started timer whenever the the user clicks on a card'
++ I declared two classes called **unmatched** and **disable** .unmatched is the class use when we click the cards which are not matched the operations present in it is executed.
++ After clicking on the each card the diable function makes it not to click again.
++ I used some logic in order to match the cards .After performing all these operations I used `starRate` function in order to give the star rating to it . Star are disappeared  when the moves are increased according to my code and I used starCount variable too.
++ I used **Restart** function to reload the game. when ever the user clicks on reload link provided on the top of the game .The game will be restarted.
++ After matching all the cards one pop menu is displayed .I displayed this pop by using **sweetalert** which contains the information like time , starRating and starCount .
---
+++ js/app.js
@@ -0,0 +1,162 @@
+"use strict";
+
+var pare = document.getElementsByClassName("deck");
+
+/*
+ * Create a list that holds all of your cards
+ */
+var sec = 0;
+var min = 0;
+var hrs = 0;
+
+var chil = document.getElementsByClassName("card");
+var chilList = Array.prototype.slice.call(chil);
+var movementSection = document.getElementById("moves");
+var time;
+let statusTime = 0
+var movements = 0;
+var cardshop = [];
+var starCount = 3;
+var stararr = [...document.getElementsByClassName("fa-star")];
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+// code for shuffling the cards
+var body = document.getElementById("body")
+body.onload = startGame();
+// code to start the game
+function startGame() {
+  var changedCards = shuffle(chilList);
+  for (var i = 0; i < changedCards.length; i++) {
+    pare[0].append(changedCards[i]);
+    console.log(changedCards[i]);
+    console.log(pare);
+  }
+}
+// Adding EventListener to the cards
+var i = 0;
+for (var i in chilList) {
+  chilList[i].addEventListener("click", displayCard);
+}
+// code for dispalying the card
+function displayCard() {
+  if (statusTime == 0)
+    timerStarts();
+  statusTime = statusTime + 1;
+  // Add required classes to the class
+  this.classList.add("card", "disable");
+  this.classList.add("open");
+  this.classList.add("show");
+  cardshop.push(this);
+  // code for moves
+  if (cardshop.length == 2) {
+    movements = movements + 1;
+    movementSection.innerHTML = movements;
+    starRate();
+    if (cardshop[0].children[0].classList.item(1) == cardshop[1].children[0].classList.item(1)) {
+      console.log("matched");
+      cardshop[0].classList.add("match", "disable");
+      cardshop[1].classList.add("match", "disable");
+      // code for when card are matched
+      if (cardsMatched.length == 16) {
+        clearInterval(time);
+        // giving pop menu to the game
+        Swal.fire({
+          title: "Congrats",
+          html: 'You have earned <strong style="color:#ff9f33; text-shadow: 3px 3px 3px #000">' + starCount + ' <i class ="fa fa-star"></i> </strong> <br> And You completed this game with the time of <br>' + hrs + 'hours :' + min + 'minutes :' + sec + 'seconds',
+          confirmButtonText: '<i class="fa fa-thumbs-up"></i>Reastrart',
+
+        }).then(() => {
+          document.location.reload();
+
+        });
+      }
+      cardshop = [];
+
+    }
+    // code when opened cards are unmatched
+    else {
+      console.log("unmatch");
+      cardshop[1].classList.add("unmatch");
+      cardshop[0].classList.add("unmatch");
+      cardshop.map((card) => {
+        setTimeout(() => {
+          card.classList.remove("unmatch", "open", "show", "disable");
+        }, 300);
+      })
+
+      cardshop = [];
+
+    }
+
+  }
+}
+var cardsMatched = document.getElementsByClassName("match");
+// timer functionality
+// giving timer to the game
+function timerStarts() {
+
+  time = setInterval(() => {
+    sec = sec + 1;
+    if (sec == 59) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hrs = hrs + 1;
+    }
+    var time = document.getElementById("time");
+
+    time.innerHTML = sec + "::" + min + "::" + hrs;
+  }, 1000)
+
+}
+
+// for refreshing the Game
+function restart() {
+  document.location.reload();
+}
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+// code for displaying card rating
+function starRate() {
+  if (movements >= 9) {
+    starCount = 2;
+    stararr[2].style.display = "none";
+
+  }
+  if (movements > 17) {
+    starCount = 1;
+    stararr[1].style.display = "none";
+  }
+
+}
